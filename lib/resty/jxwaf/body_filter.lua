67eba1d4dbd0d35b301fe95ce3c5b5d7c5f72a26
jx-sec <jx-sec@outlook.com>
2018-04-19 09:30:36 +0800

    修复
    修复

--- lib/resty/jxwaf/body_filter.lua
+++ lib/resty/jxwaf/body_filter.lua
@@ -1,18 +1,32 @@
 local waf = require "resty.jxwaf.waf"
 local config_info = waf.get_config_info()
 
-if (config_info.resp_engine == "true") and (#ngx.arg[1] ~= 0) and (ngx.arg[2] ~= true) then
+if (config_info.resp_engine == "true")  then
 	local rules = waf.get_resp_rule()
 	if #rules == 0 then
 		ngx.log(ngx.CRIT,"can not find resp rules")
 		return
 	end
+	local sign = nil
+	for _,rule in ipairs(rules) do
+			for _,match in ipairs(rule.rule_matchs) do
+					for _,var in ipairs(match.rule_vars) do
+							if var.rule_var == "RESP_HEADERS" then
+									sign = true
+							end
 
+					end
+			end
+
+	end
+
+	if sign  or ((#ngx.arg[1] ~= 0) and (ngx.arg[2] ~= true)) then		
 	local result,rule = waf.rule_match(rules)
 	
 	if result and rule.rule_action == "deny" then
 		ngx.log(ngx.ERR,"success")
 		ngx.arg[1] = nil
+	end
 	end
 --	ngx.update_time()
 --	ngx.log(ngx.ERR,ngx.now() - ngx.req.start_time())
