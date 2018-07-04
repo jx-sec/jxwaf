427b2b51696d05d903d9075166869c7fe516b2b1
jx-sec <jx-sec@outlook.com>
2018-07-04 10:39:13 +0800

    更新openresty版本
    CC新功能需要高版本openresty

--- install_waf.sh
+++ install_waf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum install pcre-devel openssl-devel gcc make g++ curl lua-devel gcc-c++ automake -y
-tar zxvf openresty-1.11.2.4.tar.gz
+tar zxvf openresty-1.13.6.2.tar.gz
 tar zxvf lua-zlib-1.2.tar.gz
-cd openresty-1.11.2.4 
+cd openresty-1.13.6.2.4 
 ./configure --prefix=/opt/jxwaf && gmake && gmake install
 cd ../libinjection/
 chmod +x src/*.py
