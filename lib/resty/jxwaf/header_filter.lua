45f9909fd35d0dd9f0c619ae9b89cd87acc72362
jx-sec <jx-sec@outlook.com>
2020-01-29 19:50:28 +0800

    Update header_filter.lua

--- lib/resty/jxwaf/header_filter.lua
+++ lib/resty/jxwaf/header_filter.lua
@@ -8,30 +8,35 @@ local req_host = update_waf_rule[host] or ngx.ctx.wildcard_host
 local request = require "resty.jxwaf.request"
 
 
-if req_host and req_host['protection_set']['owasp_protection'] == "true" and req_host['owasp_check_set']['file_traversal'] == "true" then
-  local check_time = req_host['owasp_check_set']['check_time']
-  local check_count = req_host['owasp_check_set']['check_count']
-  local check_ratio = req_host['owasp_check_set']['check_ratio']
-  local black_time = req_host['owasp_check_set']['black_time']
+if req_host and req_host['protection_set']['owasp_protection'] == "true" and req_host['owasp_check_set']['file_traversal_check'] == "true" then
+  local check_time = tonumber(req_host['owasp_check_set']['file_traversal_check_time'])
+  local check_count = tonumber(req_host['owasp_check_set']['file_traversal_check_count'])
+  local check_ratio = tonumber(req_host['owasp_check_set']['file_traversal_check_ratio'])
+  local black_time = tonumber(req_host['owasp_check_set']['file_traversal_black_time'])
   local ip_addr = request.request['REMOTE_ADDR']()
   local status = ngx.status
   local limit_bot = ngx.shared.limit_bot
   local check_file_traversal = {}
-  check_black_ip[1] = "check_file_traversal"
-  check_black_ip[2] =  ip_addr
+  check_file_traversal[1] = "check_file_traversal"
+  check_file_traversal[2] =  ip_addr
   local key_check_file_traversal = table_concat(check_file_traversal)
-  local check_file_traversal_count = limit_bot:incr(key_check_file_traversal, 1, 0, check_time) 
-  local check_not_find = {}
-  check_not_find[1] = "check_not_find"
-  check_not_find[2] =  ip_addr
-  local key_check_not_find = table_concat(check_not_find)
+  local check_file_traversal_count = limit_bot:incr(key_check_file_traversal, 1, 0, check_time)
   if status == 404 then
-    
+    local check_not_find = {}
+    check_not_find[1] = "check_not_find"
+    check_not_find[2] =  ip_addr
+    local key_check_not_find = table_concat(check_not_find)
+    local check_not_find_count = limit_bot:incr(key_check_not_find, 1, 0, check_time) 
   end
-  local not_find_count = 
-  if check_file_traversal_count and check_file_traversal_count > check_count then
-    if 
-    limit_bot:set(ip_addr,true,black_time)
+  if check_file_traversal_count > check_count then
+      local check_not_find = {}
+      check_not_find[1] = "check_not_find"
+      check_not_find[2] =  ip_addr
+      local key_check_not_find = table_concat(check_not_find)
+      local check_not_find_count = limit_bot:get(key_check_not_find) 
+      if check_not_find_count and ((check_not_find_count/check_file_traversal_count) > check_ratio) then
+        local attack_ip_check = ngx.shared.black_attack_ip
+        attack_ip_check:set(ip_addr,true,black_time)
+      end
   end
-  
 end
