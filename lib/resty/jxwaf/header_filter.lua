46776d104d99a6580c3a5322f0e77dafe8dedaf1
jx-sec <jx-sec@outlook.com>
2020-03-05 14:55:17 +0800

    Update header_filter.lua

--- lib/resty/jxwaf/header_filter.lua
+++ lib/resty/jxwaf/header_filter.lua
@@ -14,6 +14,7 @@ if req_host and req_host['protection_set']['owasp_protection'] == "true" and req
   local check_ratio = tonumber(req_host['owasp_check_set']['file_traversal_check_ratio'])
   local black_time = tonumber(req_host['owasp_check_set']['file_traversal_black_time'])
   local repeat_record = req_host['owasp_check_set']['file_traversal_repeat_record']
+  local ratio_status = req_host['owasp_check_set']['file_traversal_ratio_status'] 
   local ip_addr = request.request['REMOTE_ADDR']()
   local status = ngx.status
   local limit_bot = ngx.shared.limit_bot
@@ -48,15 +49,24 @@ if req_host and req_host['protection_set']['owasp_protection'] == "true" and req
         local check_not_find_count = limit_bot:incr(key_check_not_find, 1, 0, check_time) 
     end
   end
-  if check_file_traversal_count > check_count then
+  if ratio_status == "true" then
+    if check_file_traversal_count > check_count then
       local check_not_find = {}
       check_not_find[1] = "check_not_find"
       check_not_find[2] =  ip_addr
       local key_check_not_find = table_concat(check_not_find)
       local check_not_find_count = limit_bot:get(key_check_not_find) 
-      if check_not_find_count and ((check_not_find_count/check_file_traversal_count) > check_ratio) then
-        local attack_ip_check = ngx.shared.black_attack_ip
-        attack_ip_check:set(ip_addr,true,black_time)
+      if ratio_status  == "true" then
+        if check_not_find_count and ((check_not_find_count/check_file_traversal_count) > check_ratio) then
+          local attack_ip_check = ngx.shared.black_attack_ip
+          attack_ip_check:set(ip_addr,true,black_time)
+        end
+      else
+        if check_not_find_count and check_not_find_count > check_count then
+          local attack_ip_check = ngx.shared.black_attack_ip
+          attack_ip_check:set(ip_addr,true,black_time)
+        end
       end
+    end
   end
 end
