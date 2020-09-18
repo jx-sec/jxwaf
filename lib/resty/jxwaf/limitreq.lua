1f4b054d0d8783b1bc8bb62cc7afc2100b923a07
chenjc <jx-sec@outlook.com>
2020-09-18 21:24:45 +0800

    Update limitreq.lua

--- lib/resty/jxwaf/limitreq.lua
+++ lib/resty/jxwaf/limitreq.lua
@@ -5,16 +5,16 @@ local request = require "resty.jxwaf.request"
 local exit_code = require "resty.jxwaf.exit_code"
 _M.version = "2.0"
 
-function _M.limit_req_rate(rule,process_key)
+function _M.limit_req_rate(ip_qps,ip_expire_qps,process_key)
 	local limit_store = "limit_req"
-	local rate = tonumber(rule.rule_rate_count)
-  local burst = tonumber(rule.rule_burst_time)
+	local rate = tonumber(ip_qps)
+  local burst = tonumber(ip_expire_qps)
 	local key = process_key 
 	local lim, err = limit_req.new(limit_store, rate, burst)
 	if not lim then
     local waf_log = {}
     waf_log['log_type'] = "error"
-    waf_log['protecion_type'] = "limitreq_check"
+    waf_log['protecion_type'] = "limit_req_rate"
     waf_log['protecion_info'] = "limit_req_rate,failed to instantiate a resty.limit.req object: "..err.." limit_store is: "..limit_store
     ngx.ctx.waf_log = waf_log
 		ngx.log(ngx.ERR,"limit_req_rate,failed to instantiate a resty.limit.req object: ", err," limit_store is: ",limit_store)
@@ -23,11 +23,6 @@ function _M.limit_req_rate(rule,process_key)
 	local delay, err_incoming = lim:incoming(key, true)
 	if not delay then
 		if err_incoming == "rejected" then
-      local waf_log = {}
-      waf_log['log_type'] = "attack"
-      waf_log['protecion_type'] = "limitreq_check"
-      waf_log['protecion_info'] =  "limit_req_rate"
-      ngx.ctx.waf_log = waf_log
       return true
 		else
       local waf_log = {}
@@ -42,19 +37,20 @@ function _M.limit_req_rate(rule,process_key)
   if delay >= 0.001 then
     ngx.sleep(delay)
   end
+  return nil
 end 
 
-function _M.limit_req_count(rule,process_key)
+function _M.limit_req_count(count,black_ip_time,process_key)
 	local limit_store = "limit_req_count"
-	local count = tonumber(rule.rule_rate_count)
-	local time = tonumber(rule.rule_burst_time)
+	local limit_count = tonumber(count)
+	local limit_time = tonumber(black_ip_time)
 	local key = process_key 
-	local lim, err = limit_count.new(limit_store, count, time)
+	local lim, err = limit_count.new(limit_store, limit_count, limit_time)
 	if not lim then
     local waf_log = {}
     waf_log['log_type'] = "error"
-    waf_log['protecion_type'] = "limitreq_check"
-    waf_log['protecion_info'] = "limit_req_rate,failed to instantiate a resty.limit.count object: "..err.." limit_store is: "..limit_store
+    waf_log['protecion_type'] = "limit_req_count"
+    waf_log['protecion_info'] = "limit_req_count,failed to instantiate a resty.limit.count object: "..err.." limit_store is: "..limit_store
     ngx.ctx.waf_log = waf_log
 		ngx.log(ngx.ERR,"limit_req_count,failed to instantiate a resty.limit.count object: ", err," limit_store is: ",limit_store)
 		exit_code.return_error()
@@ -62,29 +58,24 @@ function _M.limit_req_count(rule,process_key)
 	local delay, err_incoming = lim:incoming(key, true)
 	if not delay then
 		if err_incoming == "rejected" then
-      local waf_log = {}
-      waf_log['log_type'] = "attack"
-      waf_log['protecion_type'] = "limitreq_check"
-      waf_log['protecion_info'] =  "limit_req_count"
-      ngx.ctx.waf_log = waf_log
-			--exit_code.return_limit()
       return true
 		else
       local waf_log = {}
       waf_log['log_type'] = "error"
-      waf_log['protecion_type'] = "limit_req_rate"
+      waf_log['protecion_type'] = "limit_req_count"
       waf_log['protecion_info'] =  "limit_req_count,failed to limit count: "..err_incoming
       ngx.ctx.waf_log = waf_log
       ngx.log(ngx.ERR, "limit_req_count,failed to limit count: ", err_incoming)
       exit_code.return_error()
     end
 	end
+  return nil
 end
 
-function _M.limit_req_domain_rate(rule,process_key)
+function _M.limit_req_domain_rate(domain_qps,process_key)
   local limit_store = "limit_req"
-  local rate = tonumber(rule.domain_qps)
-  local burst = tonumber(rule.domain_qps) * 0.1
+  local rate = tonumber(domain_qps)
+  local burst = 1
   local key = process_key 
   local lim, err = limit_req.new(limit_store, rate, burst)
   if not lim then
