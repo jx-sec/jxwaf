25657a9a84b8802aeccd45249df1c51316c1f513
chenjc <jx-sec@outlook.com>
2021-09-23 20:34:19 +0800

    Update jxwaf_local_init.py

--- tools/jxwaf_local_init.py
+++ tools/jxwaf_local_init.py
@@ -74,11 +74,15 @@ def main(argv):
     req_result = response.json()['result']
     print "auth result:"
     print("try to connect jxwaf server auth api_key and api_password,result is "+str(req_result))
-    print response.text
     if req_result ==  False:
         print "error message:"
         print response.text
         print response.json()['message']
+    try: 
+        error_domain = response.json()['error_domain']
+        print error_domain
+    except:
+        pass
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         usage()
