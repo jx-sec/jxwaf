84f7ef82380ded52f4c489f90998137a555fa38d
chenjc <jx-sec@outlook.com>
2024-06-05 22:22:10 +0800

    Update log.lua

--- lib/resty/jxwaf/log.lua
+++ lib/resty/jxwaf/log.lua
@@ -12,7 +12,7 @@ local ctx_waf_log = ngx.ctx.waf_log
 
 if sys_conf_data["log_conf_remote"] == "true" and (ctx_waf_log or sys_conf_data["log_all"] == "true" ) then
   local waf_log = {}
-  waf_log['host'] = ngx.var.host_http or ""
+  waf_log['host'] = ngx.var.http_host or ""
   waf_log['request_uuid'] = ngx.ctx.request_uuid
   waf_log['waf_node_uuid'] = config_info['waf_node_uuid']
   waf_log['upstream_addr'] = ngx.var.upstream_addr  or ""
