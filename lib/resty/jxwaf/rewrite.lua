c089e4b0122528a83fd3194780c3de384432bb4b
chenjc <jx-sec@outlook.com>
2024-07-04 18:23:06 +0800

    Update rewrite.lua

--- lib/resty/jxwaf/rewrite.lua
+++ lib/resty/jxwaf/rewrite.lua
@@ -4,16 +4,13 @@ local string_find = string.find
 local string_sub = string.sub
 local waf_domain_data = waf.get_waf_domain_data()
 local req_host = nil
-local dot_pos = string_find(host,".",1,true)
-local wildcard_host = nil 
-
-
 local proxy_pass_https = nil 
 
 if waf_domain_data[host] then
     req_host = waf_domain_data[host]
     proxy_pass_https = req_host['proxy_pass_https']
 else
+    local wildcard_host = nil 
     local dot_pos = string_find(host,".",1,true)
     if dot_pos then
       wildcard_host = "*"..string_sub(host,dot_pos)
